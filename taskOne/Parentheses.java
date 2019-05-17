c017f8dd4d7bb805dfcb3a52b31bd4cb75231115
Donator0081 <iliya00817@gmail.com>
2019-05-17 19:36:56 +0300

    Task one

--- taskOne/Parentheses.java
+++ taskOne/Parentheses.java
@@ -8,24 +8,36 @@ public class Parentheses {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
-        if (n <= 0) { //Consider that the entered value is greater than or equal to 0.
-            System.out.println("Number is below zero. Please, input number above zero.");
-            n = Integer.parseInt(reader.readLine());
+        if (n >= 0) { //Consider that the entered value is greater than or equal to 0.
             System.out.println(parentheses(n));
+            System.out.println(catalan(n));
         } else {
-            System.out.println(parentheses(n));
+            System.out.println("Number is below zero.");
         }
     }
 
-    public static long parentheses(int n) { // Recursive function to calculate the number Catalan
-        int[] array = new int[n + 1];// Create massive for array[i].
+    static long parentheses(int n) { // Function to calculate Catalan's number.
+        long[] array = new long[n + 1];// Creating array[i].
         array[0] = 1;
-        for (int i = 1; i <= n; i++) { // Create outer round for calculate array[n].
+        for (int i = 1; i <= n; i++) { // Create outer round to calculate array[n].
             array[i] = 0;
-            for (int j = 0; j < i; j++) { // Create inner round for calculate array[n].
+            for (int j = 0; j < i; j++) { // Create inner round to calculate array[n].
                 array[i] += array[j] * array[i - 1 - j]; // Calculate numbers of right parentheses and record this number to array[i].
             }
         }
         return array[n]; // Return number of right parentheses that recorded at array [n].
+    }
+
+    static long catalan(long n) { // Calculate catalan number with factorial.
+        return ((factorial(2 * n)) / (factorial(n) * factorial(n + 1)));
+    }
+
+    static long factorial(long number) { // Calculate factorial number.
+        long fac = 1;
+
+        for (int i = 2; i <= number; i++) {
+            fac *= i;
+        }
+        return fac;
     }
 }
