747bc5d2b3902694e51ad6f08318ad17aeb6d4cd
Donator0081 <iliya00817@gmail.com>
2019-05-17 21:38:30 +0300

    Task one

--- taskOne/Parentheses.java
+++ taskOne/Parentheses.java
@@ -9,23 +9,10 @@ public class Parentheses {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
         if (n >= 0) { //Consider that the entered value is greater than or equal to 0.
-            System.out.println(parentheses(n));
             System.out.println(catalan(n));
         } else {
             System.out.println("Number is below zero.");
         }
-    }
-
-    static long parentheses(int n) { // Function to calculate Catalan's number.
-        long[] array = new long[n + 1];// Creating array[i].
-        array[0] = 1;
-        for (int i = 1; i <= n; i++) { // Create outer round to calculate array[n].
-            array[i] = 0;
-            for (int j = 0; j < i; j++) { // Create inner round to calculate array[n].
-                array[i] += array[j] * array[i - 1 - j]; // Calculate numbers of right parentheses and record this number to array[i].
-            }
-        }
-        return array[n]; // Return number of right parentheses that recorded at array [n].
     }
 
     static long catalan(long n) { // Calculate catalan number with factorial.
